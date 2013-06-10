a9d40d64f32297363d945dccf3ad331225cc1db1
igor <igor@igor-desktop>
2013-06-10 14:54:09 +0300

    - added config for DB connetion in console

--- config/console.php
+++ config/console.php
@@ -21,6 +21,13 @@ return array(
 				),
 			),
 		),
+                'db' => array(
+                        'class' => '\yii\db\Connection',
+                        'dsn' => 'mysql:host=localhost;dbname=yii2',
+                        'username' => 'root',
+                        'password' => 'root',
+                        'charset' => 'utf8',
+                ),
 	),
 	'params' => $params,
 );
