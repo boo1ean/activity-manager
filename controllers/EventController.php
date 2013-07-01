c1cbfe1d5d8038939ff50a70139b7042fdbb274f
m1x0n <mmorozovm@gmail.com>
2013-07-01 23:03:58 +0300

    Added EventController stub

---
+++ controllers/EventController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+/**
+ * Class EventController is common controller for listing and CRUD events
+ *
+ * @package app\controllers
+ */
+class EventController extends Controller {
+
+    /**
+     * List of upcoming events in which user can take part or be invited
+     */
+    public function actionDashboard() {
+        // TODO: implement
+        echo "Upcoming events will be here";
+    }
+
+    /**
+     * User is able to create his own events
+     */
+    public function actionAdd() {
+        // TODO: implement
+        echo "New event";
+    }
+
+    /**
+     * Probably user is able to edit his event
+     *
+     * @param int|null $eventID
+     */
+    public function actionEdit($eventID = null) {
+        // TODO: implement
+        echo "Edit event";
+    }
+
+    /**
+     * Just save stuff here
+     */
+    public function actionSave() {
+        // TODO: implement
+    }
+
+    /**
+     * Also event can be deleted by user if something gonna wrong
+     *
+     * @param int|null $eventID
+     */
+    public function actionDelete($eventID = null) {
+        // TODO: implement
+        echo "Are you sure to delete this?";
+    }
+
+    /**
+     * User is able to invite other users to take part in his event
+     *
+     * @param int|null $eventID
+     */
+    public function actionInvite($eventID = null) {
+        // TODO: implement
+        echo "Invite people";
+    }
+}
+
