3d79424d33203a0f01c41fe423da4e141f04e82e
m1x0n <mmorozovm@gmail.com>
2013-07-03 00:46:48 +0300

    Added phpmailer dependency and smtp params to config

--- config/params.php
+++ config/params.php
@@ -1,5 +1,11 @@
 <?php
 
 return array(
-    'adminEmail' => 'admin@example.com',
+    'adminEmail'  => 'admin@example.com',
+
+    'smtp_host'   => 'YOUR_SMTP_HOST_HERE',
+    'smtp_user'   => 'YOUR_SMTP_USER_HERE',
+    'smtp_pwd'    => 'YOUR_SMTP_PASSWORD_HERE',
+    'smtp_port'   => 587,
+    'smtp_secure' => 'tls'
 );
