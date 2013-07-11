19ec1d6ca257876c31f99553f686465d1d0c052e
denis <denis@igor-desktop>
2013-07-11 10:40:34 +0300

    changed table schema for session table

--- migrations/m130611_130026_createSessionTable.php
+++ migrations/m130611_130026_createSessionTable.php
@@ -4,7 +4,7 @@ use \yii\db\Schema;
 class m130611_130026_createSessionTable extends \yii\db\Migration {
     public function up() {
         $this->createTable('tbl_session', array(
-            'id' => Schema::TYPE_PK,
+            'id' => Schema::TYPE_STRING,
             'expire' => Schema::TYPE_INTEGER,
             'data' => 'BLOB'
         ));
